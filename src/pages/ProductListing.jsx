1cd79ee89f77dd3924426079e50f2393d180fb79
aryan-13 <aryanchaudhary1381@gmail.com>
2022-03-29 23:35:13 +0530

    feat: Added products in DB and getData function

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {useEffect} from "react"
 import Navbar from '../Components/Navbar'
-
+import FilterBar from '../Components/FilterBar'
+import axios from 'axios';
 function ProductListing() {
+  const getData = async () => {
+    const data = await axios.get('/api/products');
+    try{
+      console.log(data.data.products);
+    }
+    catch{
+      console.log("error")
+    }
+  }
+  useEffect(() => {
+    getData();
+  });
   return (
     <div>
         <Navbar/>
+        <FilterBar/>
     </div>
   )
 }
