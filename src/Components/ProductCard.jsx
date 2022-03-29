d5b430b664d91a0bdb91f6dff90b97ef9f7756a2
aryan-13 <aryanchaudhary1381@gmail.com>
2022-03-29 23:44:57 +0530

    feat: added card component for products

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function ProductCard({product}) {
+  return (
+    <div>
+                <div className="vertical-card">
+          <div className="card-img">
+            <img src={product.img} alt="" />
+          </div>
+          <div className="card-text">
+            <h3 className="card-title">{product.name}</h3>
+            <div className="card-price">
+              {product.discountPrice} <span className="card-price-striked">{product.price}</span>
+            </div>
+          </div>
+          <div className="card-btn">
+            <div className="button-box">
+              <a href="" className="btn btn-primary btn-sm">ADD TO CART</a>
+              <i className="fa fa-shopping-cart btn-box-icon btn-sm"></i>
+            </div>
+            <a href="" className="btn btn-tertiary-icon card-btn-icon"><i className="fas fa-heart"></i></a>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
