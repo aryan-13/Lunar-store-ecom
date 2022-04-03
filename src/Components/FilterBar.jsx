c69d8d4c7449b402a6ad9a4dce08878773a18ab7
aryan-13 <aryanchaudhary1381@gmail.com>
2022-04-03 19:42:24 +0530

    fix: removed unused variables

--- src/Components/FilterBar.jsx
+++ src/Components/FilterBar.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 function FilterBar() {
 	// const [catogData, setCatogData] = useState([]);
 	// const getCatog = async () => {
